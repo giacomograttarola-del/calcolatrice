7c757f1db45642dbdb9c798133422e0cfbf5e7a6
giacomo <giacomo.grattarola@mat.tn.it>
2025-10-27 14:38:09 +0100

    completo progetto

---
+++ js/calc.js
@@ -0,0 +1,58 @@
+// funzione per la somma
+function somma(a,b) {
+    return a + b;
+}
+
+// funzione per la sottrazione
+function sottrai(a, b) {
+    return a - b;
+}
+
+// funzione per la moltiplicazione
+function moltiplica(a, b) {
+    return a * b;
+}
+
+// funzione per la divisione con gestione della divisione per zero
+function dividi(a, b) {
+    if (b === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+}
+
+// funzione di selezione dell'operazione e ritorno del valore a schermo
+function calcola(operazione) {
+    let num1 = document.getElementById('val1').value;
+    let num2 = document.getElementById('val2').value;
+    operazione = Number(operazione);
+
+    let Result;
+    switch (operazione) {
+        case 1: // somma
+            Result = somma(Number(num1), Number(num2));
+            document.getElementById('result').innerText = Result;
+            break;
+        case 3: // sottrazione
+            Result = sottrai(Number(num1), Number(num2));
+            document.getElementById('result').innerText = Result;
+            break;
+        case 2: // moltiplicazione
+            Result = moltiplica(Number(num1), Number(num2));
+            document.getElementById('result').innerText = Result;
+            break;
+        case 4: // divisione con gestione della divisione per zero (vedi dividi())
+                Result = dividi(Number(num1), Number(num2));
+                document.getElementById('result').innerText = Result;
+            break;
+        default: //default in caso di errore di tipo operazione
+            console.warn("Unknown operation:", operazione);
+    }
+}
+
+//funzione di reset dei campi
+function resetta() {
+    document.getElementById('val1').value = '';
+    document.getElementById('val2').value = '';
+    document.getElementById('result').innerText = '';
+}
